ecb34e5e2a4f5948c6219d4a85d2d8cfc59fc83f
wahidcse38 <85180005+wahidcse38@users.noreply.github.com>
2021-09-06 15:43:19 -1200

    modified header.js component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Nav, Navbar, NavbarBrand, NavItem } from 'reactstrap';
+import React, { useState } from 'react';
+import { Nav, Navbar, NavbarBrand, NavItem, Collapse, NavbarToggler } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
 import Logo from '../../assets/logo.png';
@@ -13,10 +13,14 @@ const mapStateToProps = state => {
 }
 
 const Header = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
     let links = null;
     if (props.token === null) {
         links = (
-            <Nav style={{ padding: "0 30px" }} >
+            <Nav className="ms-auto" navbar style={{ padding: "0 10px" }} >
                 <NavItem>
                     <NavLink exact to="/login" className="Navlink">Login</NavLink>
                 </NavItem>
@@ -24,7 +28,8 @@ const Header = (props) => {
         )
     } else {
         links = (
-            <Nav style={{ padding: "0 30px" }} >
+            <Nav className="ms-auto" navbar style={{ padding: "0 5px" }}>
+
                 <NavItem>
                     <NavLink exact to="/" className="Navlink">Burger-Builder</NavLink>
                 </NavItem>
@@ -39,15 +44,20 @@ const Header = (props) => {
     }
     return (
         <div className="Navigation">
-            <Navbar style={{
+            <Navbar light expand="md" style={{
                 backgroundColor: "#d70f64",
-                height: "70px"
-            }}>
+                padding: "0"
+            }} >
+                <div className="container">
+                    <NavbarBrand href="/" className="me-auto Brand">
+                        <img src={Logo} alt="Logo" width="80px" />
+                    </NavbarBrand>
 
-                <NavbarBrand href="/" className="me-auto ml-md-5 Brand">
-                    <img src={Logo} alt="Logo" width="80px" />
-                </NavbarBrand>
-                {links}
+                    <NavbarToggler onClick={toggle} />
+                    <Collapse isOpen={isOpen} navbar>
+                        {links}
+                    </Collapse>
+                </div>
             </Navbar>
         </div>
     )
